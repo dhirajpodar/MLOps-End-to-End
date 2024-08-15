e7fea665cc5010029e2a5f4cbeacfd97e8ba3872
dhirajpoddar <dhirajpodar@gmail.com>
2024-08-15 11:31:38 +0200

    training implemented

---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from DimondPricePrediction.logger.logging import logging
+from DimondPricePrediction.exception.exception import CustomException
+
+import os 
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+
+    def get_data_tranformation(self):
+        try:
+            cat_cols = ['cut','color','clarity']
+            num_cols = ['carat','depth','table', 'x','y','z']
+
+            cut_categories = ['Fair','Good','Very Good','Premium','Ideal']
+            color_categories = ['D','E','F','G','H','I','J']
+            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+
+            logging.info("Pipeline initated...")
+
+            num_pipeline = Pipeline(
+            steps=[
+                ("imputer",SimpleImputer()),
+                ("scaler",StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+            steps=[
+                ("imputer", SimpleImputer(strategy="most_frequent")),
+                ("ordinalencoder", OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                ("scaler",StandardScaler())
+                ]
+            )
+
+            preprocessor = ColumnTransformer([
+                    ("num_pipeline", num_pipeline, num_cols),
+                    ("cat_pipeline", cat_pipeline, cat_cols)
+            ])
+            return preprocessor
+
+        except Exception as e:
+            logging.info("Exception during data tranformation")
+            raise CustomException(e,sys)
+
+    def initiate_data_transformation(self,train_path,test_path):
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+
+
+        preprocessing_obj = self.get_data_tranformation()
+
+        target_col_name = 'price'
+        drop_columns = [target_col_name]
+
+        # Train data
+        input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+        target_feature_train_df = train_df[target_col_name]
+        
+        #Test data
+        input_feature_test_df = test_df.drop(columns=drop_columns,axis=1)
+        target_feature_test_df = test_df[target_col_name]
+
+        input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+        input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+        
+
+        train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+        test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+        return (train_arr,test_arr)
+
---
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -0,0 +1,39 @@
+from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_evaluation import Evaluator
+from src.DimondPricePrediction.components.model_trainer import Trainer
+
+import os
+from src.DimondPricePrediction.logger.logging import logging
+from src.DimondPricePrediction.exception.exception import CustomException
+
+class TrainingPipeline:
+    """Train pipeline."""
+
+    def start_data_ingestion(self):
+        logging.info("Data ingestion started...")
+        data_ingestion = DataIngestion()
+        train_data_path, test_data_path = data_ingestion.initate_data_ingestion()
+
+        logging.info("Data ingestion done.")
+        return train_data_path, test_data_path
+
+    def start_data_transformation(self, train_data_path, test_data_path):
+        logging.info("Data transformation started...")
+        data_transformation = DataTransformation()
+
+        train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        logging.info("Data ingestion done.")
+        return train_arr, test_arr
+
+    def start_model_training(self, train_arr, test_arr):
+        logging.info("Config model training")
+        trainer = Trainer()
+        trainer.initate_data_training(train_arr, test_arr)
+
+
+    def train(self):
+        logging.info("Training started....")
+        train_data_path, test_data_path = self.start_data_ingestion()
+        train_arr, test_arr = self.start_data_transformation(train_data_path, test_data_path)
+        self.start_model_training(train_arr,test_arr)
